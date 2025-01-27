346fbd8a7da0a959742148b04c7ccd5ff9beae99
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2025-01-27 09:07:58 +0000

    exp 1

---
+++ exp1.py
@@ -0,0 +1,5 @@
+# 1. Program to display a personalized message
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old! Have a great day!")
