31c3b7e37a5c0875ec85962d0e63ae29387f6fea
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2025-01-27 09:08:28 +0000

    exp 2

---
+++ exp2.py
@@ -0,0 +1,6 @@
+#2. Program to check if the user can vote
+
+if age >= 18:
+    print("You are eligible to vote in elections!")
+else:
+    print("You are not eligible to vote. You must be at least 18 years old.")
